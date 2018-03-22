5a494cf81dc42e15e2750991d01c34e6f26b0363
MarcelloNicoletti <marcello.s.nicoletti@gmail.com>
2018-03-22 16:48:50 -0600

    Tmux: Start sectioning logic

--- dotfiles/.tmux/status-left.sh
+++ dotfiles/.tmux/status-left.sh
@@ -10,5 +10,31 @@ PL_LEFT=$(printf "\uE0B3")
 LENGTH=$(tmux display -p "#{status-left-length}")
 SESSION=$(tmux display -p "#S")
 
+# Status values
+status=""
+size=0
+last_bg=""
+
+# the first section
+function start_section () {
+    # 1 Contents
+    # 2 Forground colour
+    # 3 Background colour
+    # 4 Extra formatting
+    status="#[fg=$2,bg=$3$4]$1"
+    last_bg="$3"
+    size=$(($size + ${#1}))
+}
+
+function end_sections () {
+    status="$status #[fg=$last_bg,bg=black]$PL_RIGHT_BLACK "
+    echo $status
+    exit 0
+}
+
+start_section "$SESSION" "colour0" "colour2" ",bold"
+
+end_sections
+
 echo "#[fg=colour0,bg=colour2,bold] $SESSION $PL_RIGHT #(whoami)@#(hostname -s) \
 #[fg=colour2,bg=colour0]$PL_RIGHT_BLACK "
