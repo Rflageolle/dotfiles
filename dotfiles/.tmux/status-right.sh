87fb304a729c3008eaebcd58fca7c58b5867679d
MarcelloNicoletti <marcello.s.nicoletti@gmail.com>
2018-03-23 00:42:45 -0600

    Tmux: Prepare right status script

--- dotfiles/.tmux/status-right.sh
+++ dotfiles/.tmux/status-right.sh
@@ -1,10 +1,27 @@
 #!/usr/bin/env bash
 
-# Powerline font glyhps
-PL_RIGHT_BLACK=$(printf "\uE0B0")
-PL_RIGHT=$(printf "\uE0B1")
-PL_LEFT_BLACK=$(printf "\uE0B2")
-PL_LEFT=$(printf "\uE0B3")
+#Powerline font glyhps
+PL_RIGHT_BLACK=$(printf "\\uE0B0")
+PL_RIGHT=$(printf "\\uE0B1")
+PL_LEFT_BLACK=$(printf "\\uE0B2")
+PL_LEFT=$(printf "\\uE0B3")
+
+# tmux values
+LEFT_STATUS_LENGTH=$(tmux display -p "#{status-left-length}")
+RIGHT_STATUS_LENGTH=$(tmux display -p "#{status-right-length}")
+CLIENT_WIDTH="$1"
+STATUS_BG=$(tmux display -p "#{status-bg}")
+
+max_width=$((CLIENT_WIDTH - LEFT_STATUS_LENGTH - 10))
+
+if [[ $RIGHT_STATUS_LENGTH -lt "$max_width" ]]; then
+    max_width="$RIGHT_STATUS_LENGTH"
+fi
+
+# Status values
+tmux_status_right=""
+cur_size=0
+last_bg=""
 
 # String parts
 TIME_STR=$(date +"%l:%M %p %Z")
