af75cda649accefe7f33f5d847b7254056987e02
MarcelloNicoletti <marcello.s.nicoletti@gmail.com>
2018-03-22 21:32:58 -0600

    Tmux: Fix bug in truncation logic

--- dotfiles/.tmux/status-left.sh
+++ dotfiles/.tmux/status-left.sh
@@ -27,8 +27,38 @@ function start_section () {
     cur_size=$((cur_size + ${#1}))
 
     if [[ $cur_size -ge $LEFT_STATUS_LENGTH ]]; then
-        difference=$((LEFT_STATUS_LENGTH - cur_size))
-        tmux_status_left=${tmux_status_left:0:difference}
+        end_of_status=$((${#tmux_status_left} - (cur_size - LEFT_STATUS_LENGTH)))
+        tmux_status_left="${tmux_status_left:0:end_of_status}"
+        end_sections
+    fi
+
+    tmux_status_left="$tmux_status_left#[none]"
+}
+
+function middle_section () {
+    # 1 Contents
+    # 2 Forground colour
+    # 3 Background colour
+    # 4 Extra formatting
+
+    cur_size=$((cur_size + 3))
+    if [[ $cur_size -ge $LEFT_STATUS_LENGTH ]]; then
+        end_sections
+    fi
+
+    if [[ $last_bg = "$3" ]]; then
+        tmux_status_left="$tmux_status_left $PL_RIGHT"
+    else
+        tmux_status_left="$tmux_status_left #[fg=$last_bg,bg=$3]$PL_RIGHT_BLACK"
+    fi
+
+    tmux_status_left="$tmux_status_left#[fg=$2,bg=$3$4] $1"
+    last_bg="$3"
+    cur_size=$((cur_size + ${#1}))
+
+    if [[ $cur_size -ge $LEFT_STATUS_LENGTH ]]; then
+        end_of_status=$((${#tmux_status_left} - (cur_size - LEFT_STATUS_LENGTH)))
+        tmux_status_left="${tmux_status_left:0:end_of_status}"
         end_sections
     fi
 
@@ -39,36 +69,11 @@ function end_sections () {
     if [[ $last_bg = "$STATUS_BG" ]]; then
         tmux_status_left="$tmux_status_left $PL_RIGHT "
     else
-        tmux_status_left="$tmux_status_left #[fg=$last_bg,bg=black]$PL_RIGHT_BLACK "
+        tmux_status_left="$tmux_status_left #[fg=$last_bg,bg=$STATUS_BG]$PL_RIGHT_BLACK "
     fi
 
     echo "$tmux_status_left#[none]"
     exit 0
-}
-
-function middle_section () {
-    # 1 Contents
-    # 2 Forground colour
-    # 3 Background colour
-    # 4 Extra formatting
-
-    if [[ $last_bg = "$3" ]]; then
-        tmux_status_left="$tmux_status_left $PL_RIGHT"
-    else
-        tmux_status_left="$tmux_status_left #[fg=$last_bg,bg=$3]$PL_RIGHT_BLACK"
-    fi
-
-    tmux_status_left="$tmux_status_left#[fg=$2,bg=$3$4] $1"
-    last_bg="$3"
-    cur_size=$((cur_size + ${#1} + 3))
-
-    if [[ $cur_size -ge $LEFT_STATUS_LENGTH ]]; then
-        difference=$((LEFT_STATUS_LENGTH - cur_size))
-        tmux_status_left=${tmux_status_left:0:difference}
-        end_sections
-    fi
-
-    tmux_status_left="$tmux_status_left#[none]"
 }
 
 start_section "$TMUX_SESSION" "colour0" "colour7" ",bold"
